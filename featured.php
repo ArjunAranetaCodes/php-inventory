f6c4eae0ac3988183656ccc713eff0b06cb48f8b
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-29 07:17:41 +0800

    2018 Updates

---
+++ featured.php
@@ -0,0 +1,16 @@
+<div class="well well-small">
+<h4>Featured Products <small class="pull-right">List of featured products</small></h4>
+<div class="row-fluid">
+<div id="featured" class="carousel slide">
+<div class="carousel-inner">
+  <div class="item active">
+  <ul class="thumbnails">
+  <?php $products->printAllFeatured();?>
+  </ul>
+  </div>
+  </div>
+  <a class="left carousel-control" href="#featured" data-slide="prev">‹</a>
+  <a class="right carousel-control" href="#featured" data-slide="next">›</a>
+  </div>
+  </div>
+</div>
