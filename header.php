7db5ab0924132a95389a1e51909de8a5f23a41bd
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-29 07:18:10 +0800

    2018 Updates

---
+++ header.php
@@ -0,0 +1,51 @@
+<meta charset="utf-8">
+<title>Lanz Furniture</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<!-- Bootstrap style -->
+<link id="callCss" rel="stylesheet" href="themes/bootshop/bootstrap.min.css" media="screen"/>
+<link href="themes/css/base.css" rel="stylesheet" media="screen"/>
+<!-- Bootstrap style responsive -->
+<link href="themes/css/bootstrap-responsive.min.css" rel="stylesheet"/>
+<link href="themes/css/font-awesome.css" rel="stylesheet" type="text/css">
+<!-- Google-code-prettify -->
+<link href="themes/js/google-code-prettify/prettify.css" rel="stylesheet"/>
+<!-- fav and touch icons -->
+<link rel="shortcut icon" href="themes/images/ico/favicon.png">
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="themes/images/ico/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="themes/images/ico/apple-touch-icon-114-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="themes/images/ico/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="themes/images/ico/apple-touch-icon-57-precomposed.png">
+<style type="text/css" id="enject"></style>
+<style type="text/css">
+  .pagination li {
+    display:inline-block;
+    border:1px solid gray;
+    border-radius:5px;
+    text-align:center;
+    vertical-align: middle;
+    margin:2px;
+    width:40px;
+    height:30px;
+  }
+
+  .pagination li a{
+    text-align:center;
+    vertical-align: middle;
+    width:40px;
+    height:30px;
+    padding:15px;
+    font-size:20px;
+    font-family:Segoe UI;
+  }
+
+  .list li{
+    list-style:none;
+  }
+
+  .name{
+    display:none;
+  }
+</style>
